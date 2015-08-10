eb9e01402d1ceec6838752768c0dd05e3151c676
obijuan <juan@iearobotics.com>
2015-08-10 08:39:26 +0200

    Identition changed

--- verilog/03-rot/rot.v
+++ verilog/03-rot/rot.v
@@ -7,21 +7,21 @@ module top(input clk, output D1, output D2, output D3, output D4, output D5);
    
    always @(posedge clk) begin
       if (ready) 
-	begin
-	   if (divider == 12000000) 
-	     begin
-		divider <= 0;
-		rot <= {rot[2:0], rot[3]};
-	     end
-	   else 
-	     divider <= divider + 1;
-	end
+				begin
+	        if (divider == 12000000) 
+	          begin
+		          divider <= 0;
+		          rot <= {rot[2:0], rot[3]};
+	          end
+	        else 
+	          divider <= divider + 1;
+	      end
       else 
-	begin
-	   ready <= 1;
-	   rot <= 4'b0001;
-	   divider <= 0;
-	end
+	      begin
+	        ready <= 1;
+	        rot <= 4'b0001;
+	        divider <= 0;
+	      end
    end
    
    assign D1 = rot[0];
