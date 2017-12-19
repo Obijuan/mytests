0f989d6f5d3e6493097e60fb02a692726bfbe95e
obijuan <juan@iearobotics.com>
2017-12-19 14:23:01 +0100

    Primer ejemplo hola mundo en javascript

---
+++ 2017-12-19-js-serial/00-js-hello-world-console/main.js
@@ -0,0 +1,5 @@
+//-- Mi primer programa en JavaScript....
+
+console.log("Probando ejemplo hola mundo 1");
+console.log("Esto va secuencial...");
+process.stdout.write('Otra forma de enviar a la consola\n');
